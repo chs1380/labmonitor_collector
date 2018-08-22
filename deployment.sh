83185816f78093435cf2e4a78b7d37af5b6eb8b9
EC2 Default User <ec2-user@ip-172-31-80-177.ec2.internal>
2018-08-22 08:22:15 +0000

    Update deployment

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,4 @@
 aws s3api create-bucket --bucket cywonglabcollectorsourcebucket --region us-east-1
+cp lambda_function/* venv/lib/python3.6/dist-packages
 sam package --template-file template.yaml --s3-bucket cywonglabcollectorsourcebucket --output-template-file package.yaml
 aws cloudformation deploy --template-file package.yaml --stack-name labmonitor --capabilities CAPABILITY_IAM
