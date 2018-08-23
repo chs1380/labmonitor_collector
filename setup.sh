8e1f6fa944989be5a3e23097cf6cadb4e220ca15
EC2 Default User <ec2-user@ip-172-31-20-131.ap-southeast-1.compute.internal>
2018-08-23 03:07:33 +0000

    Add API Key and Usage Plan

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo rm $(which sam)
+pip install --user aws-sam-cli
+sam --version
+ln -sfn $(which sam) ~/.c9/bin/sam
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
