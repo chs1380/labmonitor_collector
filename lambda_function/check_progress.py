30dcc436991265b7ec15df2b665778f81ecb8b8d
EC2 Default User <ec2-user@ip-172-31-33-170.ap-southeast-1.compute.internal>
2018-09-11 08:57:09 +0000

    API to check lab progress.

---
+++ lambda_function/check_progress.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+import os
+import datetime
+from time import gmtime, strftime
+
+
+print('Loading function')
+s3 = boto3.client('s3')
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    print(event)
+    bucket = os.environ['StudentMarkingBucket']
+    
+    studentId = event['pathParameters']['studentId']
+    lab =  event['pathParameters']['lab']
+    lab = '{:02d}'.format(int(lab))
+    prefix = f"{studentId}/lab{lab}"  
+    
+    get_filename = lambda key : os.path.split(key['Key'])[1]
+    listing = list(map(lambda key: {'file': get_filename(key), 'time': key['LastModified'].strftime('%Y-%m-%d %H:%M:%S')}, 
+                    s3.list_objects_v2(Bucket=bucket, Prefix=prefix)['Contents']))
+    return respond(None, listing)
