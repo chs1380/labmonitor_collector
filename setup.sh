5644eef1b73992d35d2bad8907d586e8f26ec088
EC2 Default User <ec2-user@ip-172-31-22-10.ap-southeast-1.compute.internal>
2018-10-12 12:53:35 +0000

    Update pip.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+curl -O https://bootstrap.pypa.io/get-pip.py 
+sudo python get-pip.py                       
+rm get-pip.py                               
 sudo yum -y update
 sudo yum -y install aws-cli
 pip install --user aws-sam-cli
