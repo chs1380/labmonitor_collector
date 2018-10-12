647332b9e0c7693fe3d4096fb44511429a598a49
EC2 Default User <ec2-user@ip-172-31-32-229.ap-southeast-1.compute.internal>
2018-10-12 08:58:41 +0000

    Fix sam 0.6

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-sudo pip install yapf
+npm uninstall -g aws-sam-local
 sudo rm $(which sam)
 pip install --user aws-sam-cli
+alias sam=~/.local/bin/sam
 sam --version
-ln -sfn $(which sam) ~/.c9/bin/sam
+ln -sfn ~/.local/bin/sam ~/.c9/bin/sam
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
