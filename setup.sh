a31c72267d7a600aff7e76119df5ea132ed21293
EC2 Default User <ec2-user@ip-172-31-44-253.ap-southeast-1.compute.internal>
2018-10-12 13:03:04 +0000

    Fix error

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 curl -O https://bootstrap.pypa.io/get-pip.py 
 sudo python get-pip.py                       
-rm get-pip.py                               
+rm get-pip.py
+npm uninstall -g aws-sam-local
+sudo rm $(which sam)
 sudo yum -y update
 sudo yum -y install aws-cli
 pip install --user aws-sam-cli
