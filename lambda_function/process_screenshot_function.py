34967998ddbfdbb7d6075c30545d7c0952787178
EC2 Default User <ec2-user@ip-172-31-43-233.ap-southeast-1.compute.internal>
2018-10-23 08:36:06 +0000

    Complete save to put item to dynamodb.

--- lambda_function/process_screenshot_function.py
+++ lambda_function/process_screenshot_function.py
@@ -2,9 +2,12 @@ import boto3
 import os
 import sys
 import uuid
+import urllib.parse
+import json
 
 rekognition_client = boto3.client('rekognition', region_name='us-east-1')
 dynamodb = boto3.resource('dynamodb')
+s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
     db = os.environ['ScreenshotMetaDataTable']
@@ -12,22 +15,20 @@ def lambda_handler(event, context):
     
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key'] 
+        key = record['s3']['object']['key']
+
+        download_path = '/tmp/{}.jpg'.format(uuid.uuid4())
         
-        response = rekognition_client.detect_text(
-                        Image={
-                            'S3Object': {
-                                'Bucket': bucket,
-                                'Name': key
-                            }
-                        }
-                    )
-        print(response)
-        
+        key = urllib.parse.unquote(key)
+        s3_client.download_file(bucket, key, download_path)
+        with open(download_path, 'rb') as image:
+            response = rekognition_client.detect_text(Image={'Bytes': image.read()})
+     
+        detected_text = response['TextDetections']
+        data ={"id": key, "DetectedText": json.dumps(detected_text)}
         db_response = table.put_item(
-           Item=response
+           Item=data
         )
-        
         print("PutItem succeeded:")
                 
        
