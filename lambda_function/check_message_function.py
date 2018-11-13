268bb5784c049128873ba9100235e7b72e22e2be
EC2 Default User <ec2-user@ip-172-31-43-233.ap-southeast-1.compute.internal>
2018-11-13 05:15:06 +0000

    Logic to delete record after student checking it.

--- lambda_function/check_message_function.py
+++ lambda_function/check_message_function.py
@@ -24,7 +24,14 @@ def get_result(student_id:str, task:str):
     id = student_id + "-" + task    
     return dynamodb_client.get_item(
                 TableName=os.environ['LabDataTable'],
-                Key={'id': {"S": str(id)}})   
+                Key={'id': {"S": str(id)}},
+                ConsistentRead=True)   
+
+def delete_result(student_id:str, task:str):
+    id = student_id + "-" + task    
+    return dynamodb_client.delete_item(
+                TableName=os.environ['LabDataTable'],
+                Key={'id': {"S": str(id)}})                   
 
 def lambda_handler(event, context):
     student_id = event['pathParameters']['studentId']
@@ -47,14 +54,17 @@ def lambda_handler(event, context):
         data = json.loads(text['Item']['DetectedText']['S'])
         line_data = filter(lambda x: x["Type"] == "LINE", data)
         texts = list(map(lambda x: {"DetectedText": x["DetectedText"]}, line_data))
+        delete_result(student_id, "TextDetections")
     if "Item" in moderation:
         moderations = json.loads(moderation['Item']['ModerationLabels']['S'])
+        delete_result(student_id, "ModerationLabels")
     if "Item" in celebrity:
         celebrities = json.loads(celebrity['Item']['CelebrityFaces']['S'])
         celebrities = list(map(lambda x: {"Name": x["Name"],"Urls":x["Urls"]}, celebrities))
+        delete_result(student_id, "CelebrityFaces")
     if "Item" in killed_processes:
         processes = json.loads(killed_processes['Item']['KilledProecess']['S'])
-        
+        delete_result(student_id, "KilledProecess")
 
     result = {'texts': texts,'moderation': moderations, "celebrity": celebrities, "process": processes}
     return respond(None, result)
