4249b13cce77a85f09fc3cb23589341f6755574f
EC2 Default User <ec2-user@ip-172-31-72-106.ec2.internal>
2018-12-18 01:13:03 +0000

    Change upgrade  sam cli for new Cloud9.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
-npm uninstall -g aws-sam-local
-sudo rm $(which sam)
 sudo yum -y update
 sudo yum -y install aws-cli
-pip install --user aws-sam-cli
 sudo -H pip install awscli --upgrade
-alias sam=~/.local/bin/sam
+pip install --user aws-sam-cli -U
 sam --version
-ln -sfn ~/.local/bin/sam ~/.c9/bin/sam
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
