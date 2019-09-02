54cb42023edddc58b22c50f257a6cde4e8f89274
EC2 Default User <ec2-user@ip-172-31-87-81.ec2.internal>
2019-09-02 05:29:57 +0000

    Try update to HK region and new version of SAM.

--- set_config.sh
+++ set_config.sh
@@ -1,2 +1,6 @@
-export STACK_NAME=lab
-export REGION=us-east-1
+export STACK_NAME=ite3101
+export REGION=ap-east-1
+# --endpoint-url https://sts.ap-east-1.amazonaws.com	
+# region=ap-east-1
+# s3 =
+#   endpoint-url = https://sts.ap-east-1.amazonaws.com
--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 sudo yum -y update
 sudo yum -y install aws-cli
 sudo -H pip install awscli --upgrade
-pip install --user aws-sam-cli -U
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+brew --version
+brew tap aws/tap
+brew install aws-sam-cli
 sam --version
 python3 -m venv venv
 source venv/bin/activate
